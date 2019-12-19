80dc5d9b8e41662460ac7b6d32df803c8e9f5dc7
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-12-19 01:58:00 -0500

    Creando los eventos del juego que se deben transmitir

---
+++ app/Events/RemainingTimeChanged.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class RemainingTimeChanged implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $time;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($time)
+    {
+        $this->time = $time;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        \Log::debug($this->time);
+
+        return new Channel('game');
+    }
+}
